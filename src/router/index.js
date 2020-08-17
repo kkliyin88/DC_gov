1b45da79255cb100bd9b9a92bf3b3b36b46ba402
kkliyin88 <leein1986@foxmail.com>
2020-08-17 18:11:32 +0800

    第三个页面

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ const Home = resolve => require(["@/page/home"], resolve)
 const about = resolve => require(["@/page/about"], resolve);
 const live = resolve => require(["@/page/live"], resolve);
 const zhaopin = resolve => require(["@/page/zhaopin"], resolve);
+const contact = resolve => require(["@/page/contact"], resolve);
 // const contact= resolve => require(["@/page/contact"], resolve);
 
 Vue.use(Router);
@@ -27,12 +28,12 @@ export const menu = [
     meta: { title: "招聘" },
   	flag:true,
   },
-  // {
-  //   path: "/contact",
-  //   component: contact,
-  //   meta: { title: "联系我们" },
-  // 	flag:true,
-  // }
+  {
+    path: "/contact",
+    component: contact,
+    meta: { title: "联系我们" },
+  	flag:true,
+  }
 ]
 export default new Router({
   routes: [
