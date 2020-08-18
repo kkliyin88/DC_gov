468692458dcdeccc2d5256447974fe66168c6399
kkliyin88 <leein1986@foxmail.com.cn>
2020-08-18 21:52:22 +0800

    guuci

--- src/page/contact/index.vue
+++ src/page/contact/index.vue
@@ -6,9 +6,26 @@
 		</section>
 	  <!-- 联系我们 -->
     <section class='box1'>
-			<div class='title'>
+			<div class='leftcontact'>
 				<img :src="require('@/assets/contact/contact us.png')" />
+        <span class='rightp1'>
+          <img :src="require('@/assets/contact/p1.png')"/>
+        </span>
 			</div>
+      <!-- 招聘负责人 -->
+     <div class='leftdbx1'>
+       <p>招聘负责人</p>
+       <img :src="require('@/assets/contact/dbx1.png')" />
+       <img :src="require('@/assets/contact/icon.png')" />
+       <span class='rightP2'>
+          <img :src="require('@/assets/contact/P2.png')" />
+       </span> 
+     </div>
+
+     <div class='leftP3'>
+        <img :src="require('@/assets/contact/P3.png')" />
+     </div>
+
 	  </section>
  </div>
 
