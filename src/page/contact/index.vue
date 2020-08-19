405c99c81f6206b0dfbeb5e0c60bdff8cae93fed
kkliyin88 <leein1986@foxmail.com>
2020-08-19 21:07:38 +0800

    111

--- src/page/contact/index.vue
+++ src/page/contact/index.vue
@@ -6,14 +6,28 @@
 		</section>
 	  <!-- 联系我们 -->
     <section class='box1'>
-			<div class='leftcontact'>
-				<img :src="require('@/assets/contact/contact us.png')" />
-        <span class='rightp1'>
-          <img :src="require('@/assets/contact/p1.png')"/>
-        </span>
-			</div>
+		<div class='title'>
+			<img :src="require('@/assets/contact/contact us.png')" />
+            <img :src="require('@/assets/contact/p1.png')"/>
+		</div>
+		<div class='content'>
+			<ul>
+				<li></li>
+				<li>
+					<img :src="require('@/assets/contact/p2.png')" />
+				</li>
+			</ul>
+			<ul>
+				<li>
+					<img :src="require('@/assets/contact/P3.png')" />
+				</li>
+				<li>
+					<img :src="require('@/assets/contact/P4.png')" />
+				</li>
+			</ul>
+		</div>
       <!-- 招聘负责人 -->
-     <div class='leftdbx1'>
+    <!-- <div class='leftdbx1'>
        <p>招聘负责人</p>
        <img :src="require('@/assets/contact/dbx1.png')" />
        <img :src="require('@/assets/contact/icon.png')" />
@@ -24,9 +38,9 @@
 
      <div class='leftP3'>
         <img :src="require('@/assets/contact/P3.png')" />
-     </div>
+     </div> -->
 
-	  </section>
+	 </section>
  </div>
 
 </template>
